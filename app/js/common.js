128975f87bc3b122bde9234dffeaf349eb85cf4e
artemghostartem <artemghostartem@gmail.com>
2021-01-06 17:27:09 +0200

    progress-bar

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
 $(function() {
-
-
-
+	$('.carouselIndexPics').owlCarousel({
+    	items: 1,
+		loop: false,
+		nav: false,
+		dots: false,
+})
+	$('.carouselIndexText').owlCarousel({
+    	items: 1,
+		loop: false,
+		nav: false,
+		dots: false,
+})
+	$('.carouselAboutText').owlCarousel({
+    	items: 1,
+		loop: false,
+		nav: false,
+		dots: false,
+})
+		$('.carouselAboutPics').owlCarousel({
+    	items: 1,
+		loop: false,
+		nav: false,
+		dots: false,
+})
+		$('.carouselAboutTastes').owlCarousel({
+    	items: 1,
+		loop: false,
+		nav: false,
+		dots: false,
+})
 });
